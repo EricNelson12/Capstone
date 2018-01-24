6cdae3e12cdbc746ce27ea7a2fee68010a12fffb
rklar3 <ravan_klar@hotmail.com>
2018-01-23 20:39:33 -0800

    newsfeed

--- Nativescript/app/tabs/tabs.component.ts
+++ Nativescript/app/tabs/tabs.component.ts
@@ -15,13 +15,20 @@ const firebaseWebApi = require("nativescript-plugin-firebase/app");
 const MapBox = require("nativescript-mapbox");
 
 //items
-import { Item } from '../items/item';
 
-export interface City {
-  country: string;
+export interface Item{
+  id:string;
+  title:string;
+  description:string;
+  adminposted:string;
+  permissions:string;
+  time:string;
+  datatype:string;
+  url: string;
   name: string;
-  population: number;
 }
+
+
 
 @Component({
   selector: "tabs",
@@ -31,24 +38,11 @@ export interface City {
 export class tabsComponent implements OnInit{
 
  //item from collection 
-  item: Item = {
-      title: '',
-      description: '',
-      adminposted: '',
-      permissions: '',
-      time: '',
-      datatype: '',
-      url: '',
-      name: '',
-    }
-  
 
-  public myCity$: Observable<City>;
-  public myCities$: Observable<Array<City>>;
-  private city: City;
-  private cities: Array<City> = [];
-
-  items: Item[];
+  public myItem$: Observable<Item>;
+  public myItems$: Observable<Array<Item>>;
+  private item: Item;
+  private items: Array<Item> = [];
 
 
   constructor(private zone: NgZone) {
@@ -69,53 +63,19 @@ export class tabsComponent implements OnInit{
   }
 
 
-
-  refreshList(args) {
-         var pullRefresh = args.object;
-         setTimeout(function () {
-            pullRefresh.refreshing = false;
-         }, 1000);
-    }
-
   firestoreCollectionObservable(): void {
-    this.myCities$ = Observable.create(subscriber => {
+    this.myItems$ = Observable.create(subscriber => {
       const colRef: firestore.CollectionReference = firebase.firestore().collection("items");
       colRef.onSnapshot((snapshot: firestore.QuerySnapshot) => {
         this.zone.run(() => {
-          this.cities = [];
-          snapshot.forEach(docSnap => this.cities.push(<City>docSnap.data()));
-          subscriber.next(this.cities);
+          this.items = [];
+          snapshot.forEach(docSnap => this.items.push(<Item>docSnap.data()));
+          subscriber.next(this.items);
         });
       });
     });
   }
 
 
-
- //function to get firebase data
- public firestoreGet(): void {
-    const collectionRef: firestore.CollectionReference = firebase.firestore().collection("items");
-    collectionRef.get()
-        .then((querySnapshot: firestore.QuerySnapshot) => {
-          querySnapshot.forEach(doc => {
-
-            //console.log("Items:  "+`${doc.id} => ${JSON.stringify(doc.data())}`);
-           
-            
-            this.item.title = doc.data().title;
-            this.item.description = doc.data().description;
-            this.item.datatype = doc.data().datatype;
-            this.item.permissions = doc.data().permissions;
-            this.item.name = doc.data().name;
-            this.item.adminposted = doc.data().adminposted;
-            this.item.time = doc.data().time;
-            this.item.url = doc.data().url;
-            console.log("Updating Data");
-
-          });
-        })
-        .catch(err => console.log());
-          //console.log("This is the second log: "+"Get failed, error" + err));         
-  }
   
 }
