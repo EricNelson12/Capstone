140f1dcdc78ba2779a8189d0347c0fee9e7572ce
Maxwell Marchand <marchandmaxwell@gmail.com>
2018-01-27 19:16:01 -0800

    Map Markers
    Embedded markers on providers page

--- Nativescript/app/tabs/tabs.component.ts
+++ Nativescript/app/tabs/tabs.component.ts
@@ -5,7 +5,11 @@ import { Image } from "tns-core-modules/ui/image";
 import { Info} from './info';
 import { registerElement } from "nativescript-angular/element-registry";
 registerElement("PullToRefresh", () => require("nativescript-pulltorefresh").PullToRefresh);
- 
+import {ElementRef, ViewChild} from '@angular/core';
+import { MapView, Marker, Position } from 'nativescript-google-maps-sdk';
+
+// Important - must register MapView plugin in order to use in Angular templates
+registerElement("MapView", () => require("nativescript-google-maps-sdk").MapView);
 
 //firebase
 import { Observable } from "rxjs/Observable";
@@ -49,6 +53,56 @@ export class tabsComponent implements OnInit{
     // AngularFireModule.initializeApp({});
   }
 
+
+latitude =  49.9298;
+longitude = -119.3968;
+zoom = 12;
+bearing = 0;
+tilt = 0;
+padding = [40, 40, 40, 40];
+mapView: MapView;
+
+lastCamera: String;
+
+
+
+//Map events
+onMapReady(event) {
+    console.log('Map Ready');
+
+    this.mapView = event.object;
+
+    console.log("Setting a marker...");
+
+    var marker = new Marker();
+    marker.position = Position.positionFromLatLng(49.9223619,-119.3971927);
+    marker.title = "Kelowna";
+    marker.snippet = "Perfit Dental";
+    marker.userData = {index: 1};
+    
+    var marker2 = new Marker();
+    marker2.position=Position.positionFromLatLng(49.9398, -119.3968);
+    marker2.title="University of British Columbia";
+    marker2.snippet = "Kelowna";
+    marker2.userData = {index:2};
+    this.mapView.addMarker(marker);
+    this.mapView.addMarker(marker2);
+}
+
+onCoordinateTapped(args) {
+    console.log("Coordinate Tapped, Lat: " + args.position.latitude + ", Lon: " + args.position.longitude, args);
+}
+
+onMarkerEvent(args) {
+    console.log("Marker Event: '" + args.eventName
+        + "' triggered on: " + args.marker.title
+        + ", Lat: " + args.marker.position.latitude + ", Lon: " + args.marker.position.longitude, args);
+}
+
+onCameraChanged(args) {
+    console.log("Camera changed: " + JSON.stringify(args.camera), JSON.stringify(args.camera) === this.lastCamera);
+    this.lastCamera = JSON.stringify(args.camera);
+}
   // initialize the firebase connection
   // get data from firestore
   ngOnInit(): void {
